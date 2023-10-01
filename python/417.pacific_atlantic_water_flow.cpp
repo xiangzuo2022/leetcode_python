a88933cb072689303d64d679e36f5aa3936db5fc
Emily Zuo <xiang.zuo@teradata.com>
2023-10-01 12:12:44 -0700

    d

--- python/417.pacific_atlantic_water_flow.cpp
+++ python/417.pacific_atlantic_water_flow.cpp
@@ -49,3 +49,36 @@ public:
 };
 
 https://www.youtube.com/watch?v=N3EHz19C0G0&list=PLwdV8xC1EWHrtgsYCcDTXIMVaHSlsnLzL&index=979&t=351s
+
+# https://www.youtube.com/watch?v=s-VkcjHqkGI
+# DFS + visit
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac, atl = set(), set()
+
+        def dfs(r, c, visit, prevHeight):
+            if ((r, c) in visit or r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < prevHeight):
+                return
+            visit.add((r, c))
+            dfs(r+1, c, visit, heights[r][c])
+            dfs(r-1, c, visit, heights[r][c])
+            dfs(r, c+1, visit, heights[r][c])
+            dfs(r, c-1, visit, heights[r][c])
+
+        # pacific and altantic columns
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS-1][c])
+
+        # pacific and altantic rows
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS-1, atl, heights[r][COLS-1])
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        return res
