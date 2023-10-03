3bf6a5767875efcb2d15a40a20c3c6873a8ec524
Emily Zuo <xiang.zuo@teradata.com>
2023-10-02 20:35:38 -0700

    D

--- python/128.longest_consecutive_sequence.py
+++ python/128.longest_consecutive_sequence.py
@@ -119,4 +119,21 @@ class Solution(object):
                 lookup[num + right] = left + right + 1
                 res = max(res, left + right + 1)
         return res
+    
+
+# https://www.youtube.com/watch?v=P6RZZMu_maU
+#
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
 
--- python/269.alien_dictionary.py
+++ python/269.alien_dictionary.py
@@ -49,3 +49,39 @@ class Solution(object):
             return ""
         # Otherwise, convert the ordering we found into a string and return it.
         return "".join(output)
+    
+
+# https://www.youtube.com/watch?v=6kTZYvNNyps
+# topological sort DAG
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adj = {c:set() for w in words for c in w}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        
+        visit = {} # False=visited, True=current path
+        res = []
+
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            visit[c] = True
+            for nei in adj[c]:
+                if dfs(nei):
+                    return True
+            visit[c] = False
+            res.append(c)
+
+        for c in adj:
+            if dfs(c):
+                return ""
+        res.reverse()
+        return "".join(res)
