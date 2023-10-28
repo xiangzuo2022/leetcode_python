871fc66e462a0dc646c0d9118157bc58569c19e0
Emily Zuo <xiang.zuo@teradata.com>
2023-10-28 14:29:08 -0700

    d

--- python/36.valid_sudoku.py
+++ python/36.valid_sudoku.py
@@ -74,6 +74,28 @@ class Solution(object):
                 if c != '.':
                     seen += [(c,j),(i,c),(i/3,j/3,c)]
         return len(seen) == len(set(seen))
+    
+
+
+# https://www.youtube.com/watch?v=TjFXEUCMqI8
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in cols[c] or
+                    board[r][c] in rows[r] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
 
 
 
--- python/42.trapping_rain_water.py
+++ python/42.trapping_rain_water.py
@@ -116,9 +116,44 @@ def trap(height):
     return trapped_water
 
 
+# https://www.youtube.com/watch?v=ZI2z5pq0TqA
+# time O(n), memeory O(1)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height: return 0
+        l, r = 0, len(height) - 1
+        res = 0
+        leftMax, rightMax = height[l], height[r]
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]                
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]                
+        return res
 
+# time O(n), memory O(1)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n <= 2:
+            return 0
+        left_max = [0] * n
+        right_max = [0] * n
+        left_max[0] = height[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], height[i])
 
-
+        right_max[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i+1], height[i])
+        res = 0
+        for i in range(n):
+            res += min(left_max[i], right_max[i]) - height[i]
+        return res
 
 
 
